3b4c6d8a4b4e055cc19dc3596fd3b49331e2cb40
AibuKuan <kuanaibu369@gmail.com>
2024-04-09 09:57:10 +0800

    fixed dbconn

--- src/model/dbConn.php
+++ src/model/dbConn.php
@@ -1,6 +1,7 @@
 <?php
 // A singleton pdo mysql connection class
-class DBConn {
+
+final class DBConn {
     private static $instance = null;
     private $conn;
 
@@ -25,38 +26,13 @@ class DBConn {
         }
     }
 
-    protected static function getInstance() {
+    public static function getInstance() {
         if (!(isset(self::$instance)))
             self::$instance = new DBConn();
         return self::$instance;
     }
 
-    protected function getConnection() {
+    public function getConnection() {
         return $this->conn;
     }
 }
-
-
-// class Test extends DBConn {
-//     public function __construct() {
-        
-//     }
-    
-//     public static function testing() {
-//         $conn = DBConn::getInstance()->getConnection();
-
-//         $sql = "INSERT INTO stdtable(stdID, stdFName, stdMName, stdLName, stdEmail) VALUES (:id, :fname, :mname, :lname, :email)";
-
-//         $stmt = $conn->prepare($sql);
-//         $stmt->execute([
-//             ':id' => 12345,
-//             ':fname' => 'Hello',
-//             ':mname' => 'Jason',
-//             ':lname' => 'Foreman',
-//             ':email' => 'jasonformat'
-//         ]);
-//     }
-// }
-
-// $test = new Test();
-// $test->testing();
