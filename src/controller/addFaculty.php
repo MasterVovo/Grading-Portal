0998f5cecde35bf2859b4527de5f4df97b4b7881
AibuKuan <kuanaibu369@gmail.com>
2024-04-18 08:24:28 +0800

    created api and model for adding faculty

---
+++ src/controller/addFaculty.php
@@ -0,0 +1,19 @@
+<?php
+// API for adding faculty
+
+require_once '../model/StudentAdder.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $fctID = $_POST['fctID'];
+    $fname = $_POST['fname'];
+    $mname = $_POST['mname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $dept = $_POST['dept'];
+    
+    $fctUpldr = new FacultyAdder($fctID, $fname, $mname, $lname, $email, $pass, $dept);
+    echo $fctUpldr->uploadToDB();
+} else {
+    exit();
+}
