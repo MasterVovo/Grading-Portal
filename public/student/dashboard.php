e60208a6fc6f5f08d5afe5742e1a543081eea5d2
Jhnllyd <johnlloydmata9@gmail.com>
2024-04-21 23:53:43 +0800

    Hirap

--- public/student/dashboard.php
+++ public/student/dashboard.php
@@ -75,45 +75,6 @@
 <body>
     <!-- Left Panel -->
     <aside id="left-panel" class="left-panel">
-        <nav class="navbar navbar-expand-sm navbar-default">
-            <div id="main-menu" class="main-menu collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="menu-title">Student:&nbsp;Testing</li>
-                    <li class="active">
-                        <a href="dashboard.html"><i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
-                    </li>
-                    
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false"> <i class="menu-icon fa fa-book"></i>Courses</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-eye"></i><a href="studentCourses.html">View Courses</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false"> <i class="menu-icon fa fa-file"></i>Result</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-eye"></i> <a href="studentResult.html"> View Result</a></li>
-                            <li><i class="fa fa-eye"></i> <a href="viewFinalResult.html"> Final Result</a></li>
-                            <li><i class="fa fa-eye"></i> <a href="gradingCriteria.html"> Grading Criteria</a></li>
-                        </ul>
-                    </li>
-                                
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Profile</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="menu-icon fa fa-user-circle"></i><a href="updateProfile.html"> Update Profile</a></li>
-                            </li>
-                        </ul>
-                        <li>
-                        <a href="logout.html"> <i class="menu-icon fa fa-power-off"></i>Logout </a>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
     </aside>
     <!-- /Left-panel -->
     <!-- Right Panel -->
@@ -300,6 +261,14 @@
     <script src="../scripts/init/fullcalendar-init.js"></script> -->
 
     <!--Local Stuff-->
+    <script>
+        fetch("includes/leftnav.html")
+          .then((response) => response.text())
+          .then((data) => {
+            document.querySelector("#left-panel").innerHTML = data;
+          });
+      </script>
+
     <script>
         $('#menuToggle').on('click', function(event) {
         var windowWidth = $(window).width();   		 
--- public/student/updateProfile.php
+++ public/student/updateProfile.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html class="no-js" lang="">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,51 +24,13 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
 </head>
+
 <body>
     <!-- Left Panel -->
     <aside id="left-panel" class="left-panel">
-        <nav class="navbar navbar-expand-sm navbar-default">
-            <div id="main-menu" class="main-menu collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="menu-title">Student:&nbsp;</li>
-                    <li class="">
-                        <a href="dashboard.html"><i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
-                    </li>
-                    
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false"> <i class="menu-icon fa fa-book"></i>Courses</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-eye"></i><a href="studentCourses.html">View Courses</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false"> <i class="menu-icon fa fa-file"></i>Result</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-eye"></i> <a href="studentResult.html"> View Result</a></li>
-                            <li><i class="fa fa-eye"></i> <a href="viewFinalResult.html"> Final Result</a></li>
-                            <li><i class="fa fa-eye"></i> <a href="gradingCriteria.html"> Grading Criteria</a></li>
-                        </ul>
-                    </li>
-                                
-                    <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false"> <i class="menu-icon fa fa-user"></i>Profile</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="menu-icon fa fa-user-circle"></i><a href="updateProfile.html"> Update Profile</a></li>
-                            </li>
-                        </ul>
-                        <li>
-                        <a href="logout.html"> <i class="menu-icon fa fa-power-off"></i>Logout </a>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
     </aside>
 
-   <!-- /#left-panel -->
+    <!-- /#left-panel -->
 
     <!-- Right Panel -->
 
@@ -87,29 +50,27 @@
                     <div class="header-left">
                         <div class="form-inline">
                             <form class="search-form">
-                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..."
-                                    aria-label="Search">
+                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
                                 <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
                             </form>
                         </div>
 
-                        
 
-                    <div class="user-area dropdown float-right">
-                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            
-                            <img class="user-avatar rounded-circle" src="../assets/img/user2.png" alt="User Avatar">
-                        </a>
 
-                        <div class="user-menu dropdown-menu">
-                            <a class="nav-link" href="updateProfile.php"><i class="fa fa-user"></i>My Profile</a>
-                            <a class="nav-link" href="logout.html"><i class="fa fa-power-off"></i>Logout</a>
+                        <div class="user-area dropdown float-right">
+                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                                <img class="user-avatar rounded-circle" src="../assets/img/user2.png" alt="User Avatar">
+                            </a>
+
+                            <div class="user-menu dropdown-menu">
+                                <a class="nav-link" href="updateProfile.php"><i class="fa fa-user"></i>My Profile</a>
+                                <a class="nav-link" href="logout.html"><i class="fa fa-power-off"></i>Logout</a>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
         </header>
         <!-- /header -->
         <!-- Header-->
@@ -151,7 +112,7 @@
                                 <!-- Credit Card -->
                                 <div id="pay-invoice">
                                     <div class="card-body">
-                                       <div class="" role="alert"></div>
+                                        <div class="" role="alert"></div>
                                         <form method="Post" action="">
                                             <div class="row">
                                                 <div class="col-6">
@@ -162,35 +123,35 @@
                                                 </div>
                                                 <div class="col-6">
                                                     <label for="x_card_code" class="control-label mb-1">Lastname</label>
-                                                        <input id="" name="lastname" type="tel" class="form-control cc-cvc" value="" Required data-val="true" data-val-required="Please enter the security code" data-val-cc-cvc="Please enter a valid security code" placeholder="Lastname">
-                                                        </div>
-                                                    </div>
-                                                    <div>
-
-                                            <div class="row">
-                                                <div class="col-6">
-                                                    <div class="form-group">
-                                                        <label for="cc-exp" class="control-label mb-1">Othername</label>
-                                                        <input id="" name="othername" type="text" class="form-control cc-exp" value="" data-val="true" data-val-required="Please enter the card expiration" data-val-cc-exp="Please enter a valid month and year" placeholder="Othername">
-                                                    </div>
-                                                </div>
-                                            <div class="col-6">
-                                                <div class="form-group">
-                                                    <label for="x_card_code" class="control-label mb-1">Email Address</label>
-                                                    <input id="" name="emailAddress" type="email" class="form-control cc-cvc" value="" Required data-val="true" data-val-required="Please enter the security code" data-val-cc-cvc="Please enter a valid security code" placeholder="Email Address">
+                                                    <input id="" name="lastname" type="tel" class="form-control cc-cvc" value="" Required data-val="true" data-val-required="Please enter the security code" data-val-cc-cvc="Please enter a valid security code" placeholder="Lastname">
                                                 </div>
                                             </div>
-                                        </div>
+                                            <div>
 
-                                        <div class="row">
-                                                <div class="col-6">
-                                                    <div class="form-group">
-                                                        <label for="cc-exp" class="control-label mb-1">Phone Number</label>
-                                                        <input id="" name="phoneNo" type="text" class="form-control cc-exp" value="" Required data-val="true" data-val-required="Please enter the card expiration" data-val-cc-exp="Please enter a valid month and year" placeholder="Phone Number">
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <div class="form-group">
+                                                            <label for="cc-exp" class="control-label mb-1">Othername</label>
+                                                            <input id="" name="othername" type="text" class="form-control cc-exp" value="" data-val="true" data-val-required="Please enter the card expiration" data-val-cc-exp="Please enter a valid month and year" placeholder="Othername">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="form-group">
+                                                            <label for="x_card_code" class="control-label mb-1">Email Address</label>
+                                                            <input id="" name="emailAddress" type="email" class="form-control cc-cvc" value="" Required data-val="true" data-val-required="Please enter the security code" data-val-cc-cvc="Please enter a valid security code" placeholder="Email Address">
+                                                        </div>
                                                     </div>
                                                 </div>
-                                           
-                                        </div>
+
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <div class="form-group">
+                                                            <label for="cc-exp" class="control-label mb-1">Phone Number</label>
+                                                            <input id="" name="phoneNo" type="text" class="form-control cc-exp" value="" Required data-val="true" data-val-required="Please enter the card expiration" data-val-cc-exp="Please enter a valid month and year" placeholder="Phone Number">
+                                                        </div>
+                                                    </div>
+
+                                                </div>
 
                                                 <button type="submit" name="submit" class="btn btn-success">Update Profile</button>
                                             </div>
@@ -200,14 +161,14 @@
                             </div>
                         </div> <!-- .card -->
                     </div><!--/.col-->
-               
 
 
-            </div>
-        </div><!-- .animated -->
-    </div><!-- .content -->
 
-    <div class="clearfix"></div>
+                </div>
+            </div><!-- .animated -->
+        </div><!-- .content -->
+
+        <div class="clearfix"></div>
 
         <!-- Footer -->
         <footer class="site-footer">
@@ -224,18 +185,18 @@
         <!-- /.site-footer -->
 
 
-</div><!-- /#right-panel -->
+    </div><!-- /#right-panel -->
 
-<!-- Right Panel -->
+    <!-- Right Panel -->
 
-<!-- Scripts -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-<script src="../assets/js/main.js"></script>
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+    <script src="../assets/js/main.js"></script>
 
-<script src="../assets/js/lib/data-table/datatables.min.js"></script>
+    <script src="../assets/js/lib/data-table/datatables.min.js"></script>
     <script src="../assets/js/lib/data-table/dataTables.bootstrap.min.js"></script>
     <script src="../assets/js/lib/data-table/dataTables.buttons.min.js"></script>
     <script src="../assets/js/lib/data-table/buttons.bootstrap.min.js"></script>
@@ -246,29 +207,37 @@
     <script src="../assets/js/lib/data-table/buttons.colVis.min.js"></script>
     <script src="../assets/js/init/datatables-init.js"></script>
 
+    <script>
+        fetch("includes/leftnav.html")
+            .then((response) => response.text())
+            .then((data) => {
+                document.querySelector("#left-panel").innerHTML = data;
+            });
+    </script>
 
     <script type="text/javascript">
         $(document).ready(function() {
-          $('#bootstrap-data-table-export').DataTable();
-      } );
+            $('#bootstrap-data-table-export').DataTable();
+        });
 
-      // Menu Trigger
-      $('#menuToggle').on('click', function(event) {
-            var windowWidth = $(window).width();   		 
-            if (windowWidth<1010) { 
-                $('body').removeClass('open'); 
-                if (windowWidth<760){ 
-                $('#left-panel').slideToggle(); 
+        // Menu Trigger
+        $('#menuToggle').on('click', function(event) {
+            var windowWidth = $(window).width();
+            if (windowWidth < 1010) {
+                $('body').removeClass('open');
+                if (windowWidth < 760) {
+                    $('#left-panel').slideToggle();
                 } else {
-                $('#left-panel').toggleClass('open-menu');  
-                } 
+                    $('#left-panel').toggleClass('open-menu');
+                }
             } else {
                 $('body').toggleClass('open');
-                $('#left-panel').removeClass('open-menu');  
-            } 
-                
-            }); 
-  </script>
+                $('#left-panel').removeClass('open-menu');
+            }
+
+        });
+    </script>
 
 </body>
+
 </html>
