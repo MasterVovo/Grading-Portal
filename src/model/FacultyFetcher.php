1084a1085bfde0cecdc4f6748b89538b40993451
AibuKuan <kuanaibu369@gmail.com>
2024-04-21 11:18:29 +0800

    removed dept col

--- src/model/FacultyFetcher.php
+++ src/model/FacultyFetcher.php
@@ -11,7 +11,7 @@ class FacultyFetcher {
     public function getAllFct() {
         $conn = DBConn::getInstance()->getConnection();
         
-        $sql = "SELECT facultyID, facultyFName, facultyMName, facultyLName, facultyEmail, facultyDept FROM faculty";
+        $sql = "SELECT facultyID, facultyFName, facultyMName, facultyLName, facultyEmail FROM faculty";
         $stmt = $conn->prepare($sql);
         $result = $stmt->execute();
 
