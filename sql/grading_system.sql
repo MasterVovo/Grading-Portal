125c065cfe405d6c621afb0c56b2b411a1e4fbe8
MasterVovo <johnandrewreyes3@gmail.com>
2024-05-03 23:07:43 +0800

    DB

--- sql/grading_system.sql
+++ sql/grading_system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2024 at 03:22 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: May 03, 2024 at 05:06 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -118,16 +118,6 @@ CREATE TABLE `assignment` (
   `courseCode` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `assignment`
---
-
-INSERT INTO `assignment` (`assignID`, `facultyID`, `sectionID`, `courseCode`) VALUES
-(2, 'fct2', 'sct2', 'GEC1000'),
-(3, 'fct3', 'sct3', 'PCIS1109'),
-(4, 'KLD125288', 'BSIS201', 'CCIS1101'),
-(5, 'KLD125288', 'BSIS101', 'GEC1000');
-
 -- --------------------------------------------------------
 
 --
@@ -146,11 +136,72 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`courseCode`, `courseName`, `courseYear`, `courseSem`) VALUES
-('CCIS1101', 'Computer Programming Lec', 1, '1'),
-('CCIS1102', 'Computer Programming Lab', 1, '1'),
-('GEC1000', 'Purposive Communication', 1, '1'),
-('PCIS1011', 'Intro to Computing', 1, '1'),
-('PCIS1109', 'Living in the IT Era', 2, '1');
+('CCIS1101', 'Intoduction to Computing Lec', 1, '1'),
+('CCIS1101L', 'Intoduction to Computing Lab', 1, '1'),
+('CCIS1102', 'Computer Programming 1 Lec', 1, '1'),
+('CCIS1102L', 'Computer Programming 1 Lab', 1, '1'),
+('CCIS1203', 'Computer Programming 2 Lec', 1, '2'),
+('CCIS1203L', 'Computer Programming 2 Lab', 1, '2'),
+('CCIS2104', 'Data Structures and Algorithm Lec', 2, '1'),
+('CCIS2104L', 'Data Structures and Algorithm Lab', 2, '1'),
+('CCIS2205', 'Information Management Lec', 2, '2'),
+('CCIS2205L', 'Information Management Lab', 2, '2'),
+('CCIS3106', 'Application Development and Emerging Tech Lec', 3, '1'),
+('CCIS3106L', 'Application Development and Emerging Tech Lab', 3, '1'),
+('GEC1000', 'Understanding the Self', 1, '2'),
+('GEC1100', 'Technical Writing', 2, '1'),
+('GEC2000', 'Ethics', 2, '2'),
+('GEC3000', 'Art Appreciation', 1, '2'),
+('GEC4000', 'Purposive Communication', 1, '1'),
+('GEC5000', 'Mathematics in the Modern World', 1, '2'),
+('GEC6000', 'The Contemporary World', 4, '1'),
+('GEC7000', 'Mga Babasahin Hinggil sa Kasaysayan ng Pilipinas', 2, '1'),
+('GEC8000', 'Science, Technology, and Society', 1, '1'),
+('GEC9100', 'Filipino 1', 2, '2'),
+('GEE1000', 'Living in the IT Era', 1, '1'),
+('GEE5000', 'People, Earth and Ecosystem', 2, '2'),
+('GEE5100', 'People, Earth and Ecosystem', 4, '1'),
+('ISCP3201', 'IS Capstone Project 1', 3, '2'),
+('ISCP4102', 'IS Capstone Project 2', 4, '1'),
+('ISMAT1200', 'Math Analysis', 2, '1'),
+('NSTP1101', 'National Training Service Program 1', 1, '1'),
+('NSTP1202', 'National Training Service Program 2', 1, '2'),
+('PCIS1101', 'Fundamentals of Information Systems', 1, '1'),
+('PCIS1102', 'Fundamentals of Database System', 1, '2'),
+('PCIS1102L', 'Fundamentals of Database System Lab', 1, '2'),
+('PCIS2103', 'Professional Issues in Information System', 2, '1'),
+('PCIS2104', 'IT Infrastructure and Network Technologies Lec', 2, '1'),
+('PCIS2104L', 'IT Infrastructure and Network Technologies Lab', 2, '1'),
+('PCIS2118', 'Web Development Lec', 2, '1'),
+('PCIS2118L', 'Web Development Lab', 2, '1'),
+('PCIS2205', 'System Analysis and Design', 2, '2'),
+('PCIS2206', 'Business Process Design and Management', 2, '2'),
+('PCIS2209', 'Organization and Management Concepts', 2, '2'),
+('PCIS2219', 'Business Law', 2, '2'),
+('PCIS3107', 'Enterprise Architecture', 3, '1'),
+('PCIS3108', 'IS Strategy, Management and Acquisition', 3, '1'),
+('PCIS3110', 'Financial Management', 3, '1'),
+('PCIS3111', 'Project Management Lec', 3, '1'),
+('PCIS3111L', 'Project Management Lab', 3, '1'),
+('PCIS3120', 'E-Commerce Mobile Application and Internet Marketing Lec', 3, '1'),
+('PCIS3120L', 'E-Commerce Mobile Application and Internet Marketing Lab', 3, '1'),
+('PCIS3121', 'Human Computer Interaction Lec', 3, '1'),
+('PCIS3121L', 'Human Computer Interaction Lab', 3, '1'),
+('PCIS3212', 'Evaluation of Business Performance', 3, '2'),
+('PCIS3213', 'Quantitative Methods', 3, '2'),
+('PCIS3214', 'Mobile Application Development Lec', 3, '2'),
+('PCIS3214L', 'Mobile Application Development Lab', 3, '2'),
+('PCIS3215', 'Internet of Things Lec', 3, '2'),
+('PCIS3215L', 'Internet of Things Lab', 3, '2'),
+('PCIS3216', 'Information System Assurance', 3, '2'),
+('PCIS3222', 'IS Technopreneurship', 3, '2'),
+('PCIS4117', 'Information Security', 4, '1'),
+('PCIS4123', 'Good Governance and Social Responsibility', 4, '1'),
+('PE1101', 'PATHFIT 1', 1, '1'),
+('PE1202', 'PATHFIT 2', 1, '2'),
+('PE2103', 'PATHFIT 3', 2, '1'),
+('PE2204', 'PATHFIT 4', 2, '2'),
+('RZL1000', 'Kursong Rizal', 1, '2');
 
 -- --------------------------------------------------------
 
@@ -174,21 +225,60 @@ CREATE TABLE `faculty` (
 --
 
 INSERT INTO `faculty` (`facultyID`, `facultyFName`, `facultyMName`, `facultyLName`, `facultyEmail`, `facultyPass`, `facultyType`, `facultyStatus`) VALUES
-('fct2', 'asdf2', 'asdf2', 'asdf2', 'asdf2@f.c', '123', 2, 3),
-('fct3', 'asdf3', 'asdf3', 'asdf3', 'asdf3@f.c', '123', 3, 3),
-('KLD1', 'John Andrew', 'Gadin', 'Reyes', 'reyes@gmail.com', '12345', 1, 1),
-('KLD125288', 'Mark Christopher', 'Pogi', 'Borja', 'borjie@gmail.com', '12345', 3, 1),
-('KLD125289', 'Cesar', 'Masipag', 'Galingana', 'galingana@gmail.com', '12345', 2, 1),
-('KLD125290', 'Cecille', 'Maganda', 'Alvaran', 'alvaran@gmail.com', '12345', 1, 1),
-('KLD125291', 'Mary Jane', 'Malupet', 'Legaspi', 'legaspi@gmail.com', '12345', 1, 1),
-('KLD125292', 'Jackie', '', 'Bostick', 'amal_horan@hotmail.com', '12345', 2, 3),
-('KLD125293', 'Belinda', '', 'Brewster', 'kit-wentz03995@ins.com', '12345', 3, 1),
-('KLD125294', 'Alba', '', 'Mcknight', 'marnie-hook@yahoo.com', '12345', 3, 1),
-('KLD125295', 'Veronique', '', 'Estrella', 'shanti.hager@bmw.com', '12345', 2, 1),
-('KLD125296', 'Kellye', '', 'Dobson-Beaudry', 'leonardo8@bid.com', '12345', 2, 1),
-('KLD125297', 'Alfredo', '', 'Han', 'soledad.drury@remains.com', '12345', 3, 1),
-('KLD2', 'John Lloyd', 'Flordeliza', 'Mata', 'mata@gmail.com', '12345', 2, 1),
-('KLD3', 'Aibu', '', 'Kuan', 'kuan@gmail.com', '12345', 3, 1);
+('admin', '', '', '', '', '12345', 0, 0),
+('KLD-23-000001', 'Abra', '', 'Kadabra', 'abracadabra@gmail.com', '123', 2, 2),
+('KLD-23-0001', 'Kellye', '', 'Dobson-Beaudry', 'leonardo8@bid.com', '12345', 2, 1),
+('KLD-23-0002', 'Alfredo', '', 'Han', 'soledad.drury@remains.com', '12345', 3, 1),
+('KLD-23-0003', 'John Lloyd', 'Flordeliza', 'Mata', 'mata@gmail.com', '12345', 2, 1),
+('KLD-23-0004', 'Aibu', '', 'Kuan', 'kuan@gmail.com', '12345', 3, 1),
+('KLD-23-0005', 'Claudia', 'Lowe', 'Galloway', 'nunc.quisque.ornare@email.com', '12345', 2, 2),
+('KLD-23-0006', 'Chaney', 'Pruitt', 'Middleton', 'sed@gmail.com', '12345', 1, 1),
+('KLD-23-0007', 'Natalie', 'Jarvis', 'Stout', 'dis@outlook.com', '12345', 2, 2),
+('KLD-23-0008', 'Amethyst', 'Garrett', 'Todd', 'ultrices@gmail.com', '12345', 2, 2),
+('KLD-23-0009', 'Colton', 'Gonzalez', 'Johnston', 'adipiscing@gmail.com', '12345', 2, 2),
+('KLD-23-0010', 'Colt', 'Morrow', 'Swanson', 'enim.nunc@email.com', '12345', 3, 1),
+('KLD-23-0011', 'Gail', 'Rutledge', 'Guerrero', 'taciti.sociosqu.ad@gmail.com', '12345', 3, 1),
+('KLD-23-0012', 'Nadine', 'Bishop', 'Dominguez', 'luctus@email.com', '12345', 4, 2),
+('KLD-23-0013', 'Kieran', 'Rosario', 'Logan', 'iaculis.aliquet@gmail.com', '12345', 4, 2),
+('KLD-23-0014', 'Sonya', 'Burgess', 'Ewing', 'dis.parturient.montes@outlook.com', '12345', 1, 1),
+('KLD-24-0001', 'John Andrew', 'Gadin', 'Reyes', 'reyes@gmail.com', '12345', 1, 1),
+('KLD-24-0002', 'Mark Christopher', 'Pogi', 'Borja', 'borjie@gmail.com', '12345', 3, 1),
+('KLD-24-0003', 'Cesar', 'Masipag', 'Galingana', 'galingana@gmail.com', '12345', 2, 1),
+('KLD-24-0004', 'Cecille', 'Maganda', 'Alvaran', 'alvaran@gmail.com', '12345', 1, 1),
+('KLD-24-0005', 'Mary Jane', 'Malupet', 'Legaspi', 'legaspi@gmail.com', '12345', 1, 1),
+('KLD-24-0006', 'Jackie', '', 'Bostick', 'amal_horan@hotmail.com', '12345', 2, 1),
+('KLD-24-0007', 'Belinda', '', 'Brewster', 'kit-wentz03995@ins.com', '12345', 3, 1),
+('KLD-24-0008', 'Alba', '', 'Mcknight', 'marnie-hook@yahoo.com', '12345', 3, 1),
+('KLD-24-0009', 'Veronique', '', 'Estrella', 'shanti.hager@bmw.com', '12345', 2, 1),
+('KLD-24-0010', 'Cally', 'Thomas', 'Clarke', 'duis.mi.enim@gmail.com', '12345', 1, 1),
+('KLD-24-0011', 'Daniel', 'Martin', 'Franklin', 'lacus@email.com', '12345', 1, 1),
+('KLD-24-0012', 'Ava', 'Villarreal', 'Hahn', 'imperdiet.erat@email.com', '12345', 2, 2),
+('KLD-24-0013', 'Holmes', 'Mcfarland', 'Bond', 'lacus.ut.nec@email.com', '12345', 2, 2),
+('KLD-24-0014', 'David', 'Hartman', 'Barrera', 'curabitur.dictum@email.com', '12345', 3, 1),
+('KLD-24-0015', 'Stephanie', 'Shields', 'Finley', 'ligula.nullam@email.com', '12345', 3, 1),
+('KLD-24-0016', 'Gavin', 'Bridges', 'Dunn', 'quam.a.felis@outlook.com', '12345', 4, 2),
+('KLD-24-0017', 'Castor', 'Parks', 'Charles', 'est.arcu@outlook.com', '12345', 4, 2),
+('KLD-24-0018', 'Cade', 'Dorsey', 'Barron', 'a.auctor.non@outlook.com', '12345', 1, 1),
+('KLD-24-0019', 'Phyllis', 'Larsen', 'Lloyd', 'cras.dolor.dolor@gmail.com', '12345', 1, 1),
+('KLD-24-0020', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', 'bjWZcWJl', 2, 3),
+('KLD-24-0021', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', 'kwxFsrhq', 1, 1),
+('KLD-24-0022', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', 'iqChiKAN', 2, 1),
+('KLD-24-0023', 'Raimondo', '', 'Cassy', 'tcassy0@cargocollective.com', '123', 1, 1),
+('KLD-24-0024', 'Bronnie', '', 'Daud', 'fdaud1@friendfeed.com', '123', 1, 1),
+('KLD-24-0025', 'Anton', '', 'Tomson', 'htomson2@ed.gov', '123', 1, 1),
+('KLD-24-0026', 'Jeffie', '', 'Le febre', 'glefebre3@yale.edu', '123', 1, 1),
+('KLD-24-0027', 'Bell', '', 'Orrocks', 'horrocks4@cyberchimps.com', '123', 1, 1),
+('KLD-24-0028', 'Blinny', 'Cynthy', 'Lunny', 'clunny5@utexas.edu', '123', 1, 1),
+('KLD-24-0029', 'Avrit', 'Charlena', 'Pedrocchi', 'cpedrocchi6@columbia.edu', '123', 1, 1),
+('KLD-24-0030', 'Sharity', 'Karee', 'Dyball', 'kdyball7@mit.edu', '123', 1, 1),
+('KLD-24-0031', 'Ferdy', 'Sonya', 'Grahame', 'sgrahame8@hibu.com', '123', 1, 1),
+('KLD-24-0032', 'Shela', '', 'Clipson', 'iclipson9@jimbo.com', '123', 1, 1),
+('KLD-24-0033', 'Conrade', 'Buddy', 'Kubanek', 'bkubanek0@umich.edu', '123', 1, 1),
+('KLD-24-0034', 'Freddy', '', 'Sweetlove', 'hsweetlove1@yahoo.com', '123', 1, 1),
+('KLD-24-0035', 'Hillie', 'Frances', 'Veillard', 'fveillard2@twitpic.com', '123', 1, 1),
+('KLD-24-0036', 'Tiphany', 'Torey', 'Storks', 'tstorks3@bbb.org', '123', 1, 1),
+('KLD-24-0037', 'Harlan', 'Pearl', 'Eley', 'peley4@bloglovin.com', '123', 1, 1),
+('KLD-24-0038', 'Ashly', '', 'Langeren', 'mlangeren5@ehow.com', '123', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -206,10 +296,11 @@ CREATE TABLE `facultytype` (
 --
 
 INSERT INTO `facultytype` (`facultyTypeID`, `facultyType`) VALUES
+(0, 'Admin'),
 (1, 'Teacher'),
 (2, 'Program Chair'),
 (3, 'Dean'),
-(4, 'registrar');
+(4, 'Registrar');
 
 -- --------------------------------------------------------
 
@@ -257,28 +348,39 @@ CREATE TABLE `section` (
 --
 
 INSERT INTO `section` (`sectionID`, `sectionAdv`, `sectionYearLvl`) VALUES
-('BSIS101', 'KLD1', 1),
-('BSIS102', 'KLD1', 1),
-('BSIS103', 'KLD1', 1),
-('BSIS104', 'KLD1', 1),
-('BSIS105', 'KLD1', 1),
-('BSIS201', 'KLD1', 2),
-('BSIS202', 'KLD1', 2),
-('BSIS203', 'KLD1', 2),
-('BSIS204', 'KLD1', 2),
-('BSIS205', 'KLD1', 2),
-('BSIS301', 'KLD1', 3),
-('BSIS302', 'KLD1', 3),
-('BSIS303', 'KLD1', 3),
-('BSIS304', 'KLD1', 3),
-('BSIS305', 'KLD1', 3),
-('BSIS401', 'KLD1', 4),
-('BSIS402', 'KLD1', 4),
-('BSIS403', 'KLD1', 4),
-('BSIS404', 'KLD1', 4),
-('BSIS405', 'KLD1', 4),
-('sct2', 'fct2', 2),
-('sct3', 'fct3', 3);
+('BSIS101', 'KLD-24-0001', 1),
+('BSIS102', 'KLD-24-0001', 1),
+('BSIS103', 'KLD-24-0001', 1),
+('BSIS104', 'KLD-24-0001', 1),
+('BSIS105', 'KLD-24-0001', 1),
+('BSIS201', 'KLD-24-0001', 2),
+('BSIS202', 'KLD-24-0001', 2),
+('BSIS203', 'KLD-24-0001', 2),
+('BSIS204', 'KLD-24-0001', 2),
+('BSIS205', 'KLD-24-0001', 2),
+('BSIS301', 'KLD-24-0001', 3),
+('BSIS302', 'KLD-24-0001', 3),
+('BSIS303', 'KLD-24-0001', 3),
+('BSIS304', 'KLD-24-0001', 3),
+('BSIS305', 'KLD-24-0001', 3),
+('BSIS401', 'KLD-24-0001', 4),
+('BSIS402', 'KLD-24-0001', 4),
+('BSIS403', 'KLD-24-0001', 4),
+('BSIS404', 'KLD-24-0001', 4),
+('BSIS405', 'KLD-24-0001', 4),
+('sct2', 'KLD-23-000001', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialization`
+--
+
+CREATE TABLE `specialization` (
+  `specializationID` int(11) NOT NULL,
+  `facultyID` varchar(20) NOT NULL,
+  `courseCode` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -293,6 +395,7 @@ CREATE TABLE `student` (
   `studentLName` varchar(50) NOT NULL,
   `studentEmail` varchar(100) NOT NULL,
   `studentPass` varchar(255) NOT NULL,
+  `studentStatus` int(11) NOT NULL,
   `studentSect` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -300,19 +403,12 @@ CREATE TABLE `student` (
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`studentID`, `studentFName`, `studentMName`, `studentLName`, `studentEmail`, `studentPass`, `studentSect`) VALUES
-('125328', 'Lucas', '', 'Yang', 'scarlet.barone@exclude.com', '12345', 'BSIS403'),
-('125329', 'Kasey', '', 'Lister', 'catherina_rogers9664@gmail.com', '12345', 'BSIS305'),
-('125330', 'Mayme', '', 'Palumbo', 'lulalake2@gmail.com', '12345', 'BSIS105'),
-('125331', 'Tambra', '', 'Matthew', 'russel-pack2@understood.com', '12345', 'BSIS204'),
-('125332', 'Karri', '', 'Lindstrom', 'zoila21718@hotmail.com', '12345', 'BSIS403'),
-('125333', 'Cayla', '', 'Putnam', 'elaina11604@yahoo.com', '12345', 'BSIS201'),
-('125334', 'Latoria', '', 'Burris', 'davisgonsalves95290@grow.com', '12345', 'BSIS202'),
-('125335', 'Katlyn', '', 'Keeling', 'krystlemilton35116@visitors.com', '12345', 'BSIS404'),
-('125336', 'Loris', '', 'Yancey', 'nelle_hand012@gmail.com', '12345', 'BSIS401'),
-('125337', 'Kristan', '', 'Daigle', 'janey.vanhorn83085@gmail.com', '12345', 'BSIS105'),
-('std2', 'asdf2', 'asdf2', 'asdf2', 'asdf2@f.c', '123', 'sct2'),
-('std3', 'asdf3', 'asdf3', 'asdf3', 'asdf3@f.c', '123', 'sct3');
+INSERT INTO `student` (`studentID`, `studentFName`, `studentMName`, `studentLName`, `studentEmail`, `studentPass`, `studentStatus`, `studentSect`) VALUES
+('KLD-24-000001', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', 'v1lSbSON', 3, 'BSIS201'),
+('KLD-24-000002', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', 'wK4GR8f2', 1, 'BSIS201'),
+('KLD-24-000003', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', 'fhMOnc30', 1, 'BSIS201'),
+('KLD-24-000004', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', '5an5PyIa', 1, 'BSIS102'),
+('KLD-24-000005', 'John Andrew', 'Gadin', 'Reyes', 'johnandrewreyes3@gmail.com', '41ucwPQB', 1, 'BSIS101');
 
 -- --------------------------------------------------------
 
@@ -344,6 +440,7 @@ CREATE TABLE `userstatus` (
 --
 
 INSERT INTO `userstatus` (`statusID`, `userStatus`) VALUES
+(0, 'admin'),
 (1, 'new'),
 (2, 'active'),
 (3, 'archive');
@@ -464,6 +561,12 @@ ALTER TABLE `section`
   ADD KEY `fksectionyear` (`sectionYearLvl`);
 
 --
+-- Indexes for table `specialization`
+--
+ALTER TABLE `specialization`
+  ADD PRIMARY KEY (`specializationID`);
+
+--
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
@@ -502,13 +605,19 @@ ALTER TABLE `assignment`
 -- AUTO_INCREMENT for table `facultytype`
 --
 ALTER TABLE `facultytype`
-  MODIFY `facultyTypeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `facultyTypeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `grade`
 --
 ALTER TABLE `grade`
   MODIFY `gradeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `specialization`
+--
+ALTER TABLE `specialization`
+  MODIFY `specializationID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `usersession`
