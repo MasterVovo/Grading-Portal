a98fbc0ec338fc1b49ba212570de974f2bcf58c7
AibuKuan <kuanaibu369@gmail.com>
2024-05-11 08:34:27 +0800

    Displays the students

--- public/scripts/teacher/finalGrades.js
+++ public/scripts/teacher/finalGrades.js
@@ -44,3 +44,40 @@ function loadCourses(event) {
     })
     .catch(error => console.error(error));
 }
+
+
+
+function loadStudents() {
+    fetch('../../src/controller/getStdList.php', {
+        method: 'POST',
+        body: (() => {
+            const formData = new FormData();
+            formData.append('method', 'getStdBySct');
+            formData.append('section', document.querySelector('#sections').value);
+            return formData;
+        })()
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('#std-tbody').innerHTML = '';
+        data.forEach(item => {
+            document.querySelector('#std-tbody').innerHTML += `
+            <tr>
+                <td>${item.studentID}</td>
+                <td>${item.studentFName + item.studentMName + item.studentLName}</td>
+                <td><input type="text" maxlength="4" size="4" /></td>
+                <td>
+                    <textarea
+                    name="feedback"
+                    id="feedback"
+                    cols="30"
+                    rows="1"
+                    ></textarea>
+                </td>
+            </tr>
+        `;
+        })
+       
+    })
+    .catch(error => console.error(error));
+}
