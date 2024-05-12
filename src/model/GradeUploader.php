68bc1d736c7f76dca542b71cedac6ae0b7f97363
Aibu Kuan <kuanaibu369@gmail.com>
2024-05-12 13:46:26 +0800

    Added approval ID when uploading grades

--- src/model/GradeUploader.php
+++ src/model/GradeUploader.php
@@ -3,6 +3,7 @@
 require_once 'DBConn.php';
 
 class GradeUploader {
+    private static $approvalID;
     public function __construct() {
         
     }
@@ -90,13 +91,22 @@ class GradeUploader {
 
     public function createRecord($stdID, $fctID, $crsCode) {
         $conn = DBConn::getInstance()->getConnection();
+
+        if (is_null(GradeUploader::$approvalID)) {
+            $sql = "INSERT INTO approval() VALUES ()";
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute();
+            GradeUploader::$approvalID = $conn->lastInsertID();
+        }
+        
     
-        $sql = "INSERT INTO grade(studentID, teacherID, courseCode) VALUES (:studentID, :facultyID, :courseCode)";
+        $sql = "INSERT INTO grade(studentID, teacherID, courseCode, gradeApproved) VALUES (:studentID, :facultyID, :courseCode, :approval)";
         $stmt = $conn->prepare($sql);
         $result = $stmt->execute([
             ':studentID' => $stdID,
             ':facultyID' => $fctID,
-            ':courseCode' => $crsCode
+            ':courseCode' => $crsCode,
+            ':approval' => GradeUploader::$approvalID
         ]);
     
         if ($result) {
