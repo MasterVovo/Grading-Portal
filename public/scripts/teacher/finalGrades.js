85831bb2d0820b4f0063e4e181554482b900d9da
Aibu Kuan <kuanaibu369@gmail.com>
2024-05-12 12:56:46 +0800

    Grade selection and input text field for feedback instead of textarea"

--- public/scripts/teacher/finalGrades.js
+++ public/scripts/teacher/finalGrades.js
@@ -69,13 +69,32 @@ function loadStudents() {
             <tr>
                 <td>${item.studentID}</td>
                 <td>${item.studentFName + ' ' + item.studentMName + ' ' + item.studentLName}</td>
-                <td><input type="text" maxlength="4" size="4" class="grades" data-id="${item.studentID}" /></td>
                 <td>
-                    <textarea
-                    class="feedbacks"
-                    cols="30"
-                    rows="1"
-                    ></textarea>
+                    <select class="custom-select form-control grades" data-id="${item.studentID}">
+                        <option disabled selected>-Grade-</option>
+                        <option value='1.00'>1.00</option>
+                        <option value='1.25'>1.25</option>
+                        <option value='1.50'>1.50</option>
+                        <option value='1.75'>1.75</option>
+                        <option value='2.00'>2.00</option>
+                        <option value='2.25'>2.25</option>
+                        <option value='2.50'>2.50</option>
+                        <option value='2.75'>2.75</option>
+                        <option value='3.00'>3.00</option>
+                        <option value='3.25'>3.25</option>
+                        <option value='3.50'>3.50</option>
+                        <option value='3.75'>3.75</option>
+                        <option value='4.00'>4.00</option>
+                        <option value='4.25'>4.25</option>
+                        <option value='4.50'>4.50</option>
+                        <option value='4.75'>4.75</option>
+                        <option value='5.00'>5.00</option>
+                        <option value='5.25'>5.25</option>
+                        <option value='5.50'>5.50</option>
+                        <option value='5.75'>5.75</option>
+                    </select>
+                <td>
+                    <input type="text" class="form-control cc-exp feedbacks">
                 </td>
             </tr>
         `;
