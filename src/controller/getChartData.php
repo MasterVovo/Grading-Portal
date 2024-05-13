fd1b98e162944b56fb6546372c3c59d3170d2975
AibuKuan <kuanaibu369@gmail.com>
2024-05-14 06:24:37 +0800

    Some chart changes

--- src/controller/getChartData.php
+++ src/controller/getChartData.php
@@ -4,17 +4,57 @@
 require_once '../model/ChartFetcher.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $method = $_POST['method'];
-
     $chartFetcher = new ChartFetcher();
 
-    if ($method == 'getActiveUsers')
-        echo $chartFetcher->getActiveUsers();
-    else if ($method == 'getTotalStudent')
-        echo $chartFetcher->getTotalStudent();
-    else if ($method == 'getTotalFaculty')
-        echo $chartFetcher->getTotalFaculty();
+    switch($_POST['method']) {
+        case 'getActiveUsers':
+            echo $chartFetcher->getActiveUsers();
+            break;
+        case 'getTotalStudent':
+            echo $chartFetcher->getTotalStudent();
+            break;
+        case 'getTotalFaculty':
+            echo $chartFetcher->getTotalFaculty();
+            break;
+        case 'getGradeHistory':
+            $gradeHistory = array();
+            $gradeHistory['firstYear']['avg'] = $chartFetcher->getAvgGrade(1);
+            $gradeHistory['secondYear']['avg'] = $chartFetcher->getAvgGrade(2);
+            $gradeHistory['thirdYear']['avg'] = $chartFetcher->getAvgGrade(3);
+            $gradeHistory['fourthYear']['avg'] = $chartFetcher->getAvgGrade(4);
 
+            $gradeHistory['firstYear']['studCount'] = $chartFetcher->countStudents(1);
+            $gradeHistory['secondYear']['studCount'] = $chartFetcher->countStudents(2);
+            $gradeHistory['thirdYear']['studCount'] = $chartFetcher->countStudents(3);
+            $gradeHistory['fourthYear']['studCount'] = $chartFetcher->countStudents(4);
+
+            $gradeHistory['firstYear']['1Y1S'] = $chartFetcher->getGradeHistory(1, 1);
+            $gradeHistory['firstYear']['1Y2S'] = $chartFetcher->getGradeHistory(1, 2);
+
+            $gradeHistory['secondYear']['1Y1S'] = $chartFetcher->getGradeHistory(1, 1);
+            $gradeHistory['secondYear']['1Y2S'] = $chartFetcher->getGradeHistory(1, 2);
+            $gradeHistory['secondYear']['2Y1S'] = $chartFetcher->getGradeHistory(2, 1);
+            $gradeHistory['secondYear']['2Y2S'] = $chartFetcher->getGradeHistory(2, 2);
+
+            $gradeHistory['thirdYear']['1Y1S'] = $chartFetcher->getGradeHistory(1, 1);
+            $gradeHistory['thirdYear']['1Y2S'] = $chartFetcher->getGradeHistory(1, 2);
+            $gradeHistory['thirdYear']['2Y1S'] = $chartFetcher->getGradeHistory(2, 1);
+            $gradeHistory['thirdYear']['2Y2S'] = $chartFetcher->getGradeHistory(2, 2);
+            $gradeHistory['thirdYear']['3Y1S'] = $chartFetcher->getGradeHistory(3, 1);
+            $gradeHistory['thirdYear']['3Y2S'] = $chartFetcher->getGradeHistory(3, 2);
+
+            $gradeHistory['fourthYear']['1Y1S'] = $chartFetcher->getGradeHistory(1, 1);
+            $gradeHistory['fourthYear']['1Y2S'] = $chartFetcher->getGradeHistory(1, 2);
+            $gradeHistory['fourthYear']['2Y1S'] = $chartFetcher->getGradeHistory(2, 1);
+            $gradeHistory['fourthYear']['2Y2S'] = $chartFetcher->getGradeHistory(2, 2);
+            $gradeHistory['fourthYear']['3Y1S'] = $chartFetcher->getGradeHistory(3, 1);
+            $gradeHistory['fourthYear']['3Y2S'] = $chartFetcher->getGradeHistory(3, 2);
+            $gradeHistory['fourthYear']['4Y1S'] = $chartFetcher->getGradeHistory(4, 1);
+            $gradeHistory['fourthYear']['4Y2S'] = $chartFetcher->getGradeHistory(4, 2);
+
+            echo json_encode($gradeHistory);
+            break;
+    }
 } else {
     exit();
 }
