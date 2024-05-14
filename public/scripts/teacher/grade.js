fa33bc2c4ee5663d3199ac1f27cf7a9ecf8dd8bb
AibuKuan <kuanaibu369@gmail.com>
2024-05-14 20:18:50 +0800

    Enhanced UX

--- public/scripts/teacher/grade.js
+++ public/scripts/teacher/grade.js
@@ -2,6 +2,9 @@ let stdDataTable = $("#std-table").DataTable({
     columnDefs: [{ targets: [2, 3], orderable: false }]
 });
 
+let rootStdList;
+let rootGrdList;
+
 function loadContent() {
     fetch('../../src/controller/getSctList.php', {
         method: 'POST',
@@ -13,7 +16,7 @@ function loadContent() {
     })
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#sections').innerHTML = '<option value="" disabled selected>Select a Section</option>';
+        document.querySelector('#sections').innerHTML = '<option value="" disabled selected>- Select a Section -</option>';
 
         data.forEach(item => {
             document.querySelector('#sections').innerHTML += `
@@ -43,7 +46,7 @@ function loadCourses(event) {
     })
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#courses').innerHTML = '<option value="" disabled selected>Select a Course</option>';
+        document.querySelector('#courses').innerHTML = '<option value="" disabled selected>- Select a Course -</option>';
 
         data.forEach(item => {
             document.querySelector('#courses').innerHTML += `
@@ -82,12 +85,28 @@ function loadStudents() {
     .then(status => {
         console.log(status)
         if (status == 'Dont exist') {
-            loadUploadingGrades();
+            fetchStdList().then(stdList => {
+                rootStdList = stdList;
+                createEditableGradeTable(stdList);
+                document.querySelector('#grade-button').innerText = 'Submit Grades';
+                document.querySelector('#grade-button').removeAttribute('disabled', 'disabled');
+                document.querySelector('#grade-button').setAttribute('onclick', 'submitGrades()');
+            });
         } else if (status == 'Approved') {
-            loadSubmittedGrades();
-        }
-        else if (status == 'Submitted') {
-            loadSubmittedGrades();
+            fetchGrdList().then(grdList => {
+                rootGrdList = grdList;
+                createUneditableGradeTable(grdList);
+                document.querySelector('#grade-button').innerText = 'Cannot Edit';
+                document.querySelector('#grade-button').setAttribute('disabled', 'disabled');
+            });
+        } else if (status == 'Submitted') {
+            fetchGrdList().then(grdList => {
+                rootGrdList = grdList;
+                createUneditableGradeTable(grdList);
+                document.querySelector('#grade-button').innerText = 'Edit';
+                document.querySelector('#grade-button').removeAttribute('disabled', 'disabled');
+                document.querySelector('#grade-button').setAttribute('onclick', 'turnToEditableGradeTable()');
+            });
         }
     })
     .catch(error => console.error(error));
@@ -110,8 +129,25 @@ function createGradeDataTable() {
 
 
 
-function loadSubmittedGrades() {
-    fetch('../../src/controller/getGrades.php', {
+async function fetchStdList() {
+    return await fetch('../../src/controller/getStdList.php', {
+        method: 'POST',
+        body: (() => {
+            const formData = new FormData();
+            formData.append('method', 'getStdBySct');
+            formData.append('section', document.querySelector('#sections').value);
+            return formData;
+        })()
+    })
+    .then(response => response.json())
+    .then(data => {
+       return data;
+    })
+    .catch(error => console.error(error));
+}
+
+async function fetchGrdList() {
+    return await fetch('../../src/controller/getGrades.php', {
         method: 'POST',
         body: (() => {
             const formData = new FormData();
@@ -123,72 +159,84 @@ function loadSubmittedGrades() {
     })
     .then(response => response.json())
     .then(data => {
-        clearGradeTable();
-        data.forEach(item => {
-            document.querySelector('#std-tbody').innerHTML += `
-            <tr>
-                <td>${item.studentID}</td>
-                <td>${item.studentFName + ' ' + item.studentMName + ' ' + item.studentLName}</td>
-                <td>${item.gradeMidterm}</td>
-                <td>${item.gradeFeedback}</td>
-            </tr>
-        `;
-        });
-        createGradeDataTable();
-       
+        return data;
     })
     .catch(error => console.error(error));
 }
 
-function loadUploadingGrades() {
-    fetch('../../src/controller/getStdList.php', {
-        method: 'POST',
-        body: (() => {
-            const formData = new FormData();
-            formData.append('method', 'getStdBySct');
-            formData.append('section', document.querySelector('#sections').value);
-            return formData;
-        })()
-    })
-    .then(response => response.json())
-    .then(data => {
-        clearGradeTable();
-        data.forEach(item => {
-            document.querySelector('#std-tbody').innerHTML += `
-            <tr>
-                <td>${item.studentID}</td>
-                <td>${item.studentFName + ' ' + item.studentMName + ' ' + item.studentLName}</td>
-                <td>
-                    <select class="custom-select form-control grades" data-id="${item.studentID}">
-                        <option disabled selected>-Grade-</option>
-                        <option value='1.00'>1.00</option>
-                        <option value='1.25'>1.25</option>
-                        <option value='1.50'>1.50</option>
-                        <option value='1.75'>1.75</option>
-                        <option value='2.00'>2.00</option>
-                        <option value='2.25'>2.25</option>
-                        <option value='2.50'>2.50</option>
-                        <option value='2.75'>2.75</option>
-                        <option value='3.00'>3.00</option>
-                        <option value='3.25'>3.25</option>
-                        <option value='3.50'>3.50</option>
-                        <option value='3.75'>3.75</option>
-                        <option value='4.00'>4.00</option>
-                        <option value='4.25'>4.25</option>
-                        <option value='4.50'>4.50</option>
-                        <option value='4.75'>4.75</option>
-                        <option value='5.00'>5.00</option>
-                    </select>
-                <td>
-                    <input type="text" class="form-control cc-exp feedbacks">
-                </td>
-            </tr>
-        `;
-        });
-        createGradeDataTable();
-       
-    })
-    .catch(error => console.error(error));
+
+
+function turnToEditableGradeTable() {
+    createEditableGradeTable(rootGrdList);
+
+    document.querySelector('#grade-button').innerText = 'Submit';
+    document.querySelector('#grade-button').setAttribute('onclick', 'submitGrades()');
+
+    document.querySelector('#cancel-button').removeAttribute('hidden', 'hidden');
+}
+
+function turnToUneditableGradeTable() {
+    createUneditableGradeTable(rootGrdList);
+
+    document.querySelector('#grade-button').innerText = 'Edit';
+    document.querySelector('#grade-button').setAttribute('onclick', 'turnToEditableGradeTable()');
+
+    document.querySelector('#cancel-button').setAttribute('hidden', 'hidden');
+}
+
+
+function createUneditableGradeTable(grdList) {
+    clearGradeTable();
+
+    grdList.forEach(item => {
+        document.querySelector('#std-tbody').innerHTML += `
+        <tr>
+            <td>${item.studentID}</td>
+            <td>${item.studentFName + ' ' + item.studentMName + ' ' + item.studentLName}</td>
+            <td>${item.gradeMidterm}</td>
+            <td>${item.gradeFeedback}</td>
+        </tr>
+    `;
+    });
+    createGradeDataTable();
+}
+
+function createEditableGradeTable(stdList) {
+    clearGradeTable();
+    
+    stdList.forEach(item => {
+        document.querySelector('#std-tbody').innerHTML += `
+        <tr>
+            <td>${item.studentID}</td>
+            <td>${item.studentFName + ' ' + item.studentMName + ' ' + item.studentLName}</td>
+            <td>
+                <select class="custom-select form-control grades" data-id="${item.studentID}">
+                    <option disabled selected>-Grade-</option>
+                    <option value='1.00'>1.00</option>
+                    <option value='1.25'>1.25</option>
+                    <option value='1.50'>1.50</option>
+                    <option value='1.75'>1.75</option>
+                    <option value='2.00'>2.00</option>
+                    <option value='2.25'>2.25</option>
+                    <option value='2.50'>2.50</option>
+                    <option value='2.75'>2.75</option>
+                    <option value='3.00'>3.00</option>
+                    <option value='3.25'>3.25</option>
+                    <option value='3.50'>3.50</option>
+                    <option value='3.75'>3.75</option>
+                    <option value='4.00'>4.00</option>
+                    <option value='4.25'>4.25</option>
+                    <option value='4.50'>4.50</option>
+                    <option value='4.75'>4.75</option>
+                    <option value='5.00'>5.00</option>
+                </select>
+            <td>
+                <input type="text" class="form-control cc-exp feedbacks">
+            </td>
+        </tr>
+    `;
+    });
+    createGradeDataTable();
 }
 
 
@@ -202,38 +250,41 @@ function submitGrades() {
         showCancelButton: true,
       })
       .then((result) => {
-        const gradesElement = document.querySelectorAll('.grades');
-        const feedbacksElement = document.querySelectorAll('.feedbacks');
-
-        let assocArr = {};
-        for (let i = 0; i < gradesElement.length; i++) {
-            assocArr[gradesElement[i].getAttribute('data-id')] = {'grade': gradesElement[i].value, 'feedback': feedbacksElement[i].value};
+        if (result.isConfirmed) {
+            const gradesElement = document.querySelectorAll('.grades');
+            const feedbacksElement = document.querySelectorAll('.feedbacks');
+    
+            let assocArr = {};
+            for (let i = 0; i < gradesElement.length; i++) {
+                assocArr[gradesElement[i].getAttribute('data-id')] = {'grade': gradesElement[i].value, 'feedback': feedbacksElement[i].value};
+            }
+    
+            fetch('../../src/controller/setGrades.php', {
+                method: 'POST',
+                body: (() => {
+                    const formData = new FormData();
+                    formData.append('term', document.querySelector('#term').value);
+                    formData.append('grades', JSON.stringify(assocArr));
+                    formData.append('course', document.querySelector('#courses').value);
+                    return formData;
+                })()
+            })
+            .then(response => response.json())
+            .then(data => {
+                swal.fire({
+                    title: data,
+                    icon: "success",
+                    showConfirmButton: true,
+                })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = "grade.html";
+                    }
+                })
+            })
+            .catch(error => console.error(error));
         }
-
-        fetch('../../src/controller/setGrades.php', {
-            method: 'POST',
-            body: (() => {
-                const formData = new FormData();
-                formData.append('term', document.querySelector('#term').value);
-                formData.append('grades', JSON.stringify(assocArr));
-                formData.append('course', document.querySelector('#courses').value);
-                return formData;
-            })()
-        })
-        .then(response => response.json())
-        .then(data => {
-            swal.fire({
-                title: data,
-                icon: "success",
-                showConfirmButton: true,
-            })
-            .then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = "grade.html";
-                }
-            })
-        })
-        .catch(error => console.error(error));
+        
     });
 
 
